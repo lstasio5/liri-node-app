22f64815d8b8974d1a22c150b6f8617d4d20abd4
Lisa Stasio <lstasio@sgintl.com>
2019-03-13 14:09:26 -0400

    pushing changes to github

---
+++ javascript/liri.js
@@ -0,0 +1,48 @@
+
+require("dotenv").config();
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var axios = require("axios");
+//var fs = require("fs");
+
+//"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+
+var artist = process.argv.slice(3).join(" ")
+console.log(artist);
+
+axios
+  .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+  .then(function(response) {
+
+    console.log(response.data);
+    // Having issues with getting certain data points to display- node giving me an error
+    //console.log(response.venue.name);
+  }
+
+)
+//node liri.js spotify-this-song '<song name here>'
+
+
+ 
+var songName = process.argv.slice(3).join(" ");
+ 
+var spotify = new Spotify(keys.spotify);
+
+
+spotify
+  .search({ type: 'track', query: songName })
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+ 
+spotify.search({ type: 'track', query: songName }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
